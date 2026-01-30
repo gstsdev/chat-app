5067916004ca22a97467d2f0af5c3ae8a7c5e6e2
Gustavo Sampaio <gustavosamirts@gmail.com>
2026-01-30 16:51:47 -0300

    feat(pages/chat): group messages by sender in chat history

--- src/pages/chat/ChatMessageHistory.tsx
+++ src/pages/chat/ChatMessageHistory.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import type { ChatMessage } from "@/model/Chat";
 
 interface ChatMessageHistoryProps {
@@ -7,20 +8,36 @@ interface ChatMessageHistoryProps {
 export default function ChatMessageHistory({
   messages,
 }: ChatMessageHistoryProps) {
+  const messagesBySender = messages?.reduce<
+    { sender: string; messages: ChatMessage[] }[]
+  >((acc, message) => {
+    if (acc.at(-1)?.sender == message.sender) {
+      acc.at(-1)?.messages.push(message);
+    } else {
+      acc.push({ sender: message.sender, messages: [message] });
+    }
+
+    return acc;
+  }, []);
+
   return (
     <div className="bg-white rounded-md shadow-md size-full overflow-y-auto min-h-0 flex flex-col">
       <div className="h-fit mt-auto">
-        {messages?.map((message) => (
-          <div key={message.id} className="w-full py-4 px-5">
-            {message.sender === "You" ? (
-              <div className="ml-auto p-2 rounded-xl bg-indigo-500 text-white w-fit h-fit">
+        {messagesBySender?.map(({ sender, messages }, idx) => (
+          <div key={idx} className="w-full py-4 px-5 space-y-2">
+            {messages.map((message) => (
+              <div
+                key={message.id}
+                className={cn(
+                  "p-2 rounded-xl size-fit",
+                  sender == "You"
+                    ? "ml-auto bg-indigo-500 text-white"
+                    : "mr-auto bg-neutral-200 text-black",
+                )}
+              >
                 <p>{message.content}</p>
               </div>
-            ) : (
-              <div className="mr-auto p-2 rounded-xl bg-neutral-200 text-black w-fit h-fit">
-                <p>{message.content}</p>
-              </div>
-            )}
+            ))}
           </div>
         ))}
       </div>
